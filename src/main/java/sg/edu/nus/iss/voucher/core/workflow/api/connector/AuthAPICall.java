9fae5e108359a7686a45e863e9c249053edeeeff
KhinYadanarNg <e0015793@u.nus.edu>
2025-04-08 22:52:40 +0800

    removed unused imported packages

--- src/main/java/sg/edu/nus/iss/voucher/core/workflow/api/connector/AuthAPICall.java
+++ src/main/java/sg/edu/nus/iss/voucher/core/workflow/api/connector/AuthAPICall.java
@@ -10,13 +10,11 @@ import org.springframework.beans.factory.annotation.Value;
 
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 
