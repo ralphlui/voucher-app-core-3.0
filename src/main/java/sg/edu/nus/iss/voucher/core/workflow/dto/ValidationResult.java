436f88dc017bed32b796762bc03ed79288087c5a
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-04-10 03:21:45 +0800

    Removed unused method.

--- src/main/java/sg/edu/nus/iss/voucher/core/workflow/dto/ValidationResult.java
+++ src/main/java/sg/edu/nus/iss/voucher/core/workflow/dto/ValidationResult.java
@@ -3,17 +3,16 @@ package sg.edu.nus.iss.voucher.core.workflow.dto;
 import org.springframework.http.HttpStatus;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ValidationResult {
 	private boolean isValid;
 	private String message;
 	private String imageUrl;
 	private HttpStatus status;
 
-	public ValidationResult() {
-
-	}
 }
