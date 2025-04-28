c51c4faa2a639f80ef25279d85c9cc90dbe166e0
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-04-29 01:21:25 +0800

    Update the HTMLSanitizerUtil.

--- src/main/java/sg/edu/nus/iss/voucher/core/workflow/utility/HtmlSanitizerUtil.java
+++ src/main/java/sg/edu/nus/iss/voucher/core/workflow/utility/HtmlSanitizerUtil.java
@@ -1,15 +1,12 @@
 package sg.edu.nus.iss.voucher.core.workflow.utility;
 
-import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 
 public class HtmlSanitizerUtil {
 
-	private static final PolicyFactory POLICY = new HtmlPolicyBuilder()
-			.disallowElements("html", "script", "svg", "iframe", "object", "embed", "details", "math")
-	        .toFactory();
-	
-	 
+	private static final PolicyFactory POLICY = Sanitizers.BLOCKS;
+		 
 
     public static String sanitize(String htmlInput) {
         return POLICY.sanitize(htmlInput);
